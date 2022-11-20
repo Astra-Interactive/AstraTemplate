b7d1c362c4332252f7d07b9fc137da972ec580ec
makeevrserg <makeevrserg@gmail.com>
2022-11-20 14:42:27 +0300

    Fabric AstraLibs dependency

--- fabric/src/main/java/ru/astrainteractive/astratemplate/ClientEntryPoint.kt
+++ fabric/src/main/java/ru/astrainteractive/astratemplate/ClientEntryPoint.kt
@@ -1,7 +1,11 @@
 package ru.astrainteractive.astratemplate
 
 import net.fabricmc.api.ClientModInitializer
+import ru.astrainteractive.astralibs.di.module
 
+val HelloWorldModule = module {
+    "Hello world"
+}
 class ClientEntryPoint : ClientModInitializer {
     override fun onInitializeClient() {
         println("AstraTemplate: onInitializeClient")
--- fabric/src/main/java/ru/astrainteractive/astratemplate/command/HelloWorldCommand.kt
+++ fabric/src/main/java/ru/astrainteractive/astratemplate/command/HelloWorldCommand.kt
@@ -2,13 +2,14 @@ package ru.astrainteractive.astratemplate.command
 
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.server.command.ServerCommandSource
+import ru.astrainteractive.astratemplate.HelloWorldModule
 
 
 object HelloWorldCommand : ICommand("helloworld") {
 
 
     override fun onCommand(context: CommandContext<ServerCommandSource>): Int {
-        println("Hello world!")
+        println(HelloWorldModule.value)
         return 1
     }
 
