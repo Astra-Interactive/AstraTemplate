03e527a87d047548ad153e156efdb5ff368890e8
Roman Makeev <makeevrserg@gmail.com>
2023-10-31 15:33:52 +0300

    fix: gradle

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -14,16 +14,9 @@ dependencies {
     implementation(libs.minecraft.astralibs.orm)
     implementation(libs.minecraft.astralibs.spigot.gui)
     implementation(libs.minecraft.astralibs.spigot.core)
-    // klibs
-//    implementation(libs.bundles.klibs)
     // Spigot dependencies
     compileOnly(libs.minecraft.paper.api)
     implementation(libs.minecraft.bstats)
-    // Test
-//    testImplementation(platform(libs.tests.junit.bom))
-//    testImplementation(libs.bundles.testing.libs)
-//    testImplementation(libs.bundles.testing.kotlin)
-//    testImplementation(libs.minecraft.mockbukkit)
     // Local
     implementation(projects.modules.apiLocal)
     implementation(projects.modules.apiRemote)
